d5897865ffcc21c993e65cc70d5a97d847b711a4
Codesworth <codezworth@gmail.com>
2019-02-15 11:12:26 +0000

    Initial Commit

---
+++ Material-Dialogs-iOS/Material_Dialogs_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Material_Dialogs_iOS.h
+//  Material-Dialogs-iOS
+//
+//  Created by Shadrach Mensah on 15/02/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Material_Dialogs_iOS.
+FOUNDATION_EXPORT double Material_Dialogs_iOSVersionNumber;
+
+//! Project version string for Material_Dialogs_iOS.
+FOUNDATION_EXPORT const unsigned char Material_Dialogs_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Material_Dialogs_iOS/PublicHeader.h>
+
+
