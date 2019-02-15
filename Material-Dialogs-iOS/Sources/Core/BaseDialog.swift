514765360e5495a13493e20f848bf4ca81c1daea
Codesworth <codezworth@gmail.com>
2019-02-15 11:54:07 +0000

    Initial Commit. Initialized Base and Core

---
+++ Material-Dialogs-iOS/Sources/Core/BaseDialog.swift
@@ -0,0 +1,50 @@
+//
+//  BaseDialog.swift
+//  Material-Dialogs-iOS
+//
+//  Created by Shadrach Mensah on 15/02/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+import UIKit
+
+internal class BaseDialog:UIView{
+    
+    private var nativeFrame: CGRect = {
+        return UIScreen.main.bounds
+    }()
+    
+    private var allowableSize:CGSize{
+        return CGSize(width: CGFloat(nativeFrame.width * 0.9), height: CGFloat(nativeFrame.height * 0.8))
+    }
+    
+    private var overlay:UIView = {
+        let overlay = UIView.init(frame: .zero)
+        overlay.backgroundColor = .lightGray
+        overlay.alpha = 0.7
+        return overlay
+    }()
+    
+    
+    init(){
+        super.init(frame: nativeFrame)
+        self.frame = nativeFrame
+        self.backgroundColor = .clear
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        overlay.frame = nativeFrame
+        addSubview(overlay)
+    }
+    
+    override func addSubview(_ view: UIView) {
+        fatalError("Cannot add subview to this, Use defined method")
+    }
+    
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Material-Dialogs-iOS/Sources/MaterialDialog/MaterialDialog.swift
@@ -0,0 +1,28 @@
+//
+//  MaterialDialog.swift
+//  Material-Dialogs-iOS
+//
+//  Created by Shadrach Mensah on 15/02/2019.
+//  Copyright © 2019 Shadrach Mensah. All rights reserved.
+//
+
+import UIKit
+
+
+
+public class MaterialDialog{
+    
+    var base:BaseDialog = {
+        return BaseDialog()
+    }()
+    
+    public func show()->MaterialDialog{
+        
+    }
+}
+
+
+
+public class BasicDialog{
+    
+}
