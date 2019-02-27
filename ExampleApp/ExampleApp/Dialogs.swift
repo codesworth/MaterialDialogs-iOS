50cebb09cc507e7b97d8476433d2591c6a42097b
Codesworth <codezworth@gmail.com>
2019-02-27 13:14:20 +0000

    Created Custom View

--- ExampleApp/ExampleApp/Dialogs.swift
+++ ExampleApp/ExampleApp/Dialogs.swift
@@ -150,36 +150,37 @@ class Dialogs{
         names = ["Basic Dialog", "Text Input Dialog", "Text Input Group Dialog","Progressive Dialog", "Infinite Progress Dialog","Infinite Progress Dialog (No Footer)", "Single List Dialog", "Multiple List Dialog", "Custom View"]
     }
     
+    var colorInfo:(color:UIColor,position:Int)?
+    
+    @objc func colorThis(_ button:ColorButtons){
+        colorInfo = (button.color,button.position)
+    }
+    
     func createView()->MaterialDialog{
         let cview = UIView(frame: CGRect(origin: .zero, size: CGSize(width:UIScreen.main.bounds.width * 0.9
-            , height:400)))
-        let imageView = UIImageView(frame: .zero)
-        imageView.image = UIImage(named: "Vampire Toucan.jpg")
-        imageView.clipsToBounds = true
-        imageView.contentMode = .scaleAspectFill
-        let lable = UILabel(frame: .zero)
-        lable.text = "Boring Boring ha"
-        lable.textColor = .blue
-        cview.addSubview(imageView)
-        cview.addSubview(lable)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        lable.translatesAutoresizingMaskIntoConstraints = false
+            , height:200)))
+        let colorStck = UIStackView(frame: CGRect(origin: .zero, size: CGSize(width: UIScreen.main.bounds.width * 0.9, height: 100)))
+        colorStck.distribution = .fillEqually
+        colorStck.alignment = .center
+        colorStck.axis = .horizontal
+        for i in 1...4{
+            let color = UIColor.getRandom()
+            let spot = ColorButtons(frame: CGRect(origin: .zero, size: CGSize(width: 40, height: 40)), color: color, position: i)
+            spot.addTarget(self, action: #selector(colorThis(_:)), for: .touchUpInside)
+            colorStck.addSubview(spot)
+        }
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: cview.topAnchor),
-            imageView.leadingAnchor.constraint(equalTo: cview.leadingAnchor, constant: 8),
-            imageView.trailingAnchor.constraint(equalTo: cview.trailingAnchor, constant: -20),
-            imageView.heightAnchor.constraint(equalToConstant: 200),
-            lable.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
-            lable.centerXAnchor.constraint(equalTo: cview.centerXAnchor),
-            lable.heightAnchor.constraint(equalToConstant: 30)
-            
-            ])
+            colorStck.leadingAnchor.constraint(equalTo: cview.leadingAnchor, constant: 16),
+            colorStck.trailingAnchor.constraint(equalTo: cview.trailingAnchor, constant: -16),
+            colorStck.heightAnchor.constraint(equalToConstant: 50),
+            colorStck.topAnchor.constraint(equalTo: cview.topAnchor, constant: 20)
+        ])
         
         let custom = CustomDialog(view: cview) { () -> Any in
-            return lable.text ?? ""
+            return self.colorInfo ?? (.black,-1)
         }
         
-        let dialog = MaterialDialog.customDialog(title: "Custommyy", customview: custom, withFooter: true) { (type) in
+        let dialog = MaterialDialog.customDialog(title: "Pick A Color", customview: custom, withFooter: true) { (type) in
             switch type{
             case .affirm(let result):
                 guard let result = result as? String else {
@@ -198,4 +199,41 @@ class Dialogs{
     }
     
     
+}
+
+
+class ColorButtons:UIButton{
+    
+    var color:UIColor = .black
+    
+    var position:Int = -1
+    
+    
+    init(frame: CGRect, color:UIColor,position:Int) {
+        super.init(frame: frame)
+        self.color = color
+        self.position = position
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.width / 2
+        layer.masksToBounds = true
+        backgroundColor = color
+    }
+    
+    
+}
+
+
+extension UIColor{
+    
+    class func getRandom()->UIColor{
+        let colors:[UIColor] = [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1), #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1),#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1), #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1),#colorLiteral(red: 0.3565862775, green: 0.8337638974, blue: 0.8115196824, alpha: 1)]
+        return colors.randomElement()!
+    }
 }
